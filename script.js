f836ba44015801aa4d69ed7dd917ad165de52504
Gift Chika <giftchika222@gmail.com>
2023-08-06 20:54:07 -0600

    updates

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let getBtn = document.querySelector('.getBtn');
+let menu = document.querySelector('.menu');
+let removeBtn = document.querySelector('.removeBtn');
+// let open = document.querySelector('.open')
 
-getBtn.addEventListener('click',function(){
-    
-})
+getBtn.addEventListener("click",function(){
+    menu.classList.toggle("open");
+});
+
+// let Btn = document.querySelector('.btn');
+// let men = document.querySelector('.men');
+// // let open = document.querySelector('.open')
+
+// Btn.addEventListener("click",function(){
+//     men.classList.toggle("me");
+// });
+
+
+removeBtn.addEventListener("click", function(){
+    menu.classList.toggle("menu");
+});
